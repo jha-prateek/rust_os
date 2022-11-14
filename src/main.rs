05a791738a346422b2a577223d305a077da73b57
Prateek Jha <prateek.jha@udaan.com>
2022-11-14 23:20:30 +0530

    Custom test setup

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,42 @@
 #![no_std]
 #![no_main]
 
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 mod vga_buffer;
 
 use core::panic::PanicInfo;
 
-/// This function is called on panic.
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info);
     loop {}
 }
 
-static HW_STRING: &[u8] = b"Hello from Rust OS Kernel";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello from Rust OS Kernel v{}", 1.0);
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
