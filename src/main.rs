57da535c72ab0c31d71788288dab61ea98c72a31
Prateek Jha <prateek.jha@udaan.com>
2022-11-14 23:41:40 +0530

    Bootstrapping basic test functionality

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,9 @@
 
 mod vga_buffer;
 
+use x86_64::instructions::port::Port;
 use core::panic::PanicInfo;
+use crate::QemuExitCode::Success;
 
 
 #[panic_handler]
@@ -26,12 +28,28 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
     println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
+    exit_qemu(Success);
 }
 
 #[test_case]
