b42cc9bb312541db1568f83c3e10e850d89e1b14
Prateek Jha <prateek.jha@udaan.com>
2022-11-15 01:27:21 +0530

    Integration Tests setup

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,13 @@
 #![no_main]
 
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 mod serial;
 
 use core::panic::PanicInfo;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -27,10 +20,7 @@ fn panic(_info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", _info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
+    rust_os::test_panic_handler(_info);
 }
 
 #[no_mangle]
@@ -42,43 +32,3 @@ pub extern "C" fn _start() -> ! {
 
     loop {}
 }
-
-fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-    where T: Fn() {
-    fn run(&self) -> () {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-//
-// #[test_case]
-// fn trivial_assertion() {
-//     assert_eq!(1, 1);
-// }
-//
-// #[test_case]
-// fn fail() {
-//     assert_eq!(1, 0);
-// }
